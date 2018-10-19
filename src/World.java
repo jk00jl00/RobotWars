136578d377afa2cbe0d1319eb56ae1de5b1d96f9
bjlu16 <luddenr1@live.se>
2018-10-19 13:14:45 +0200

    Started on implemented world

--- src/World.java
+++ src/World.java
@@ -2,13 +2,12 @@ import World;
 import Controller;
 import Robot;
 
-public class World
-{
+public class World {
     /** Attributes */
     /**
      * 
      */
-    public  ;
+
     /**
      * 
      */
@@ -36,7 +35,7 @@ public class World
     /**
      * 
      */
-    private int columns;
+    private int collumns;
     /** Associations */
     private Robot unnamed_1;
     /**
@@ -57,7 +56,12 @@ public class World
      * @param rows - 
      * @return World
      */
-    public World  ( int collumns, int rows ){}
+    public World  ( int collumns, int rows ){
+        this.collumns = collumns;
+        this.rows = rows;
+
+        generateWorld();
+    }
     /**
      * Operation generateWorld
      *
